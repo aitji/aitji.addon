a8d70aa1fd83f8d280f4b08c9d96ea90d5409a28
InwAitJi <100911929+lnwAitJi@users.noreply.github.com>
2023-07-18 14:30:01 +0700

    Delete 1.20/Room Chat directory

--- 1.20/Room Chat/Addon/scripts/Look/Reload.js
+++
@@ -1,5 +0,0 @@
-import { world } from "@minecraft/server";
-/** ----------------------------------------------- */
-world.afterEvents.worldInitialize.subscribe(() => {
-    world.sendMessage(`§l§8| §r§fServer has been §cReloaded!!`)
-})
