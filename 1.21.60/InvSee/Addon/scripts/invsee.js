d49e039fed94e5ad14651758933eb6175fec5b5f
aitji <ait.suriya@gmail.com>
2025-02-13 21:08:32 +0700

    1.18.0-beta without test sorry! im busy

---
+++ 1.21.60/InvSee/Addon/scripts/invsee.js
@@ -0,0 +1,61 @@
+import * as Minecraft from "@minecraft/server"
+const World = Minecraft.world
+
+export function invsee(message, args) {
+    if (typeof message !== "object") throw TypeError(`message is type of ${typeof message}. Expected "object".`);
+
+    const player = message.sender
+    if (args.length === 0) return player.endMessage("§7You need to provide whos §finventory §7to view!§r")
+
+    for (const pl of World.getPlayers()) if (pl.name.toLowerCase().includes(args[0].toLowerCase().replace(/"|\\|@/g, ""))) {
+        var member = pl
+        break
+    }
+
+    if (typeof member === "undefined") return player.endMessage("§7Couldn't find that §fplayer.§r")
+    const container = member.getComponent('inventory').container
+
+    if (container.size === container.emptySlotsCount) {
+        player.endMessage(`§r§f${member.name}'s§7 inventory is empty.§r`)
+        return
+    }
+
+    let inventory = `§f${member.name}'s §7inventory:§r\n\n`
+
+    for (let i = 0; i < container.size; i++) {
+        const item = container.getItem(i)
+        if (typeof item === "undefined") continue
+        let str = item.typeId
+
+        str = str.replace("minecraft:", "").replace(/_/g, " ").toLowerCase().replace(/\b[a-z]/g, function (letter) {
+            return letter.toUpperCase()
+        })
+        let str2 = str
+        str2 = str.replace("Pa:", "").replace(/_/g, " ").toLowerCase().replace(/\b[a-z]/g, function (letter) {
+            return letter.toUpperCase()
+        })
+        inventory += `§r§7Slot §f${i}§7: §f${str2}:§r§o${item.nameTag || '§r'}§r §7x${item.amount}§r\n`
+
+            const loopIterator = (iterator) => {
+                const iteratorResult = iterator.next()
+                if (iteratorResult.done === true) return
+                const enchantData = iteratorResult.value
+
+                let enchantmentName = enchantData.type.id
+                enchantmentName = enchantmentName.charAt(0).toUpperCase() + enchantmentName.slice(1)
+
+                inventory += `§7       | §f${enchantmentName}§7:§f${enchantData.level}§r\n`
+
+                loopIterator(iterator)
+            }
+            loopIterator(item.getComponent("enchantments").enchantments[Symbol.iterator]())
+    }
+    player.endMessage(`§r§c§l§ฟ*-*-*-*-*-*-*-*-*§r\n${inventory}§r§c§l§ฟ*-*-*-*-*-*-*-*-*§r`)
+}
+
+/**
+ * @author "aitji."
+ * @youtube AitJi Gamer
+ * @copyright 2022-2023
+ * @settings เปิด Beta API ด้วยนะ :>
+ */
---
+++ 1.21.60/NearChat/Addon/scripts/main.js
@@ -0,0 +1,137 @@
+/** ________________________________________________________ */
+import { world, ScoreboardIdentity, ScoreboardIdentityType, system, Player } from "@minecraft/server"
+import { ActionFormData, ModalFormData, MessageFormData } from "@minecraft/server-ui"
+system.beforeEvents.watchdogTerminate.subscribe((data) => data.cancel = true)
+world.sendMessage(`§l§8| §r§fDistance Chat Now §cReload!!`)
+/** ________________________________________________________ */
+function getScore(objective, target, useZero = true) {
+    try {
+        const obj = world.scoreboard.getObjective(objective)
+        if (typeof target == 'string') {
+            return obj.getScore(obj.getParticipants().find(v => v.displayName == target))
+        }
+        return obj.getScore(target.scoreboardIdentity)
+    } catch {
+        return useZero ? 0 : NaN
+    }
+}
+function toBool(data) {
+    if (data === 0) return false
+    if (data === 1) return true
+    return false
+}
+function boolTo(data) {
+    if (data === false) return 0
+    if (data === true) return 1
+    return 0
+}
+/** ________________________________________________________ */
+createScore("chatDistance")
+function createScore(scoreboardName) {
+    if (world.scoreboard.getObjective(scoreboardName)) return
+    world.scoreboard.addObjective(scoreboardName, scoreboardName)
+}
+/** ________________________________________________________ */
+function CalDistance(pl1, pl2) {
+    const dx = pl1.x - pl2.x;
+    const dy = pl1.y - pl2.y;
+    const dz = pl1.z - pl2.z;
+
+    return Math.sqrt(dx * dx + dy * dy + dz * dz);
+}
+/** ________________________________________________________ */
+world.beforeEvents.itemUse.subscribe(data => {
+    const pl = data.source
+    const item = data.itemStack
+
+    if (item.typeId === "minecraft:paper" && pl.hasTag("Admin")) menu(pl)
+})
+/** ________________________________________________________ */
+world.beforeEvents.chatSend.subscribe(data => {
+    let radius, messageToggle, AdminToggle, TellAdminToggle
+    try {
+        messageToggle = toBool(getScore("chatDistance", "messageToggle", true)) ?? false
+        AdminToggle = toBool(getScore("chatDistance", "AdminToggle", true)) ?? false
+        radius = getScore("chatDistance", "chatRang", true) ?? false
+        TellAdminToggle = toBool(getScore("chatDistance", "TellAdminToggle", true)) ?? false
+    } catch (UwU) { }
+    if (radius === "" || radius === undefined || radius < 1) radius = "15"
+    if (messageToggle === "" || messageToggle === undefined) messageToggle = false
+    if (AdminToggle === "" || AdminToggle === undefined) AdminToggle = false
+    if (TellAdminToggle === "" || TellAdminToggle === undefined) TellAdminToggle = false
+
+    const pl = data.sender;
+    const msg = data.message;
+    data.cancel = true
+    const plPosition = pl.location;
+    if (pl.hasTag("Admin") && TellAdminToggle) {
+        world.sendMessage(`<${pl.name}> ${msg}`)
+        return
+    }
+    pl.sendMessage(`§f <${pl.name}> ${msg}§r`)
+    world.getAllPlayers().filter(plrs => pl.name !== plrs.name).forEach(plr => {
+        if (plr.hasTag("Admin") && AdminToggle) {
+            plr.sendMessage(`<${pl.name}> ${msg}`)
+            return
+        }
+        if (plr !== pl) {
+            const plrPosition = plr.location;
+            const distance = CalDistance(plPosition, plrPosition);
+
+            if (distance <= Number(radius)) plr.sendMessage(`<${pl.name}> ${msg}`)
+            else {
+                if (messageToggle) plr.sendMessage(`§l§f${pl.name} ได้§cส่ง§fข้อความ..`)
+                else return
+            }
+        }
+    });
+});
+/** ________________________________________________________ */
+// Hover pl = world.getAllPlayers().filter(plr = plr.hasTag("Admin"))[0]
+function menu(pl) {
+    system.run(() => {
+        let chatRang, messageToggle, AdminToggle, TellAdminToggle
+        try {
+            messageToggle = toBool(getScore("chatDistance", "messageToggle", true))
+            AdminToggle = toBool(getScore("chatDistance", "AdminToggle", true))
+            chatRang = getScore("chatDistance", "chatRang", true)
+            TellAdminToggle = toBool(getScore("chatDistance", "TellAdminToggle", true))
+        } catch (UwU) { }
+        if (chatRang === "" || chatRang === undefined || chatRang < 1) chatRang = "15"
+        if (messageToggle === "" || messageToggle === undefined) messageToggle = false
+        if (AdminToggle === "" || AdminToggle === undefined) AdminToggle = false
+        if (TellAdminToggle === "" || TellAdminToggle === undefined) TellAdminToggle = false
+
+        const form = new ModalFormData()
+        form.title(`§l§8» §r§7NearChat§8 «`)
+        form.textField(`§c» §cSubscribe §f@aitji.\n\n§l§a» §fกำหนดระยะของแชท:\n§7» §iไม่สามารถน้อยกว่า 0 ได้..`, `ex: "13"`, `${chatRang}`)
+        form.toggle(`§l§fขึ้นข้อความเมื่อไม่ได้รับ§eข้อความ§fไหม§f:`, messageToggle)
+        form.toggle(`§l§f"Admin" เห็นทุก§aข้อความ§f:`, AdminToggle)
+        form.toggle(`§l§f"Admin" พิมพ์แล้ว§aเห็น§fทุกคน§f:`, TellAdminToggle)
+        form.show(pl).then(res => {
+            if (res.canceled) return
+            const resu = res.formValues
+            if (res.formValues[0] === "" || res.formValues[0] === undefined) {
+                pl.sendMessage(`§l§fกรุณากรอก§eข้อมูล§fให้ครบถ้วน`)
+                return
+            }
+            if (resu[0] < 1) {
+                pl.sendMessage(`§l§fระยะของ §eข้อความ §fไม่สามารถน้อยกว่า 0 ได้`)
+                return
+            }
+            pl.runCommandAsync(`scoreboard players reset * chatDistance`)
+            pl.runCommandAsync(`scoreboard players set chatRang chatDistance 15`)
+            pl.runCommandAsync(`scoreboard players set messageToggle chatDistance 0`)
+            pl.runCommandAsync(`scoreboard players set AdminToggle chatDistance 0`)
+            pl.runCommandAsync(`scoreboard players set TellAdminToggle chatDistance 0`)
+
+            pl.runCommandAsync(`scoreboard players set chatRang chatDistance ${resu[0]}`)
+            pl.runCommandAsync(`scoreboard players set messageToggle chatDistance ${boolTo(resu[1])}`)
+            pl.runCommandAsync(`scoreboard players set AdminToggle chatDistance ${boolTo(resu[2])}`)
+            pl.runCommandAsync(`scoreboard players set TellAdminToggle chatDistance ${boolTo(resu[3])}`)
+
+            pl.sendMessage(`§fChat Distance is now §aSaved!§r`)
+        })
+    })
+}
+/** ________________________________________________________ */
